f1a28e7aabf2278367daa4ead5270f56202001cd
Alexandra Ioan <alexandra.ioan@skywindgroup.com>
2024-11-15 15:21:11 +0200

    rename docker file

--- src/db/models/role.model.js
+++ src/db/models/role.model.js
@@ -38,7 +38,7 @@ module.exports = (sequelize, DataTypes) => {
 
 	role.associate = (models) => {
 		role.hasMany(models.user, {
-			foreignKey: 'id',
+			foreignKey: 'rold_id',
 			onDelete: 'CASCADE',
 		});
 	};
